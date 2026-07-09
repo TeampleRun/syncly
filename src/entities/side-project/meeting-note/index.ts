e8740ef0fb4b7a425fe6c45283a34da82c332545
Kwon812 <tre723@naver.com>
2026-07-09 13:46:58 +0900

    fix: 대시보드 최근 회의록 위젯 엔티티 수정

--- src/entities/side-project/meeting-note/index.ts
+++
@@ -1,3 +0,0 @@
-// meeting-note 엔티티의 Public API (모델)
-export { type MeetingNote } from './model/meeting-note.types';
-export { mockMeetingNotes } from './model/meeting-note.mock';
--- src/entities/side-project/meeting-note/model/meeting-note.types.ts
+++
@@ -1,8 +0,0 @@
-// 회의록(MeetingNote) 도메인 모델
-export interface MeetingNote {
-  id: string;
-  title: string;
-  date: string;
-  /** 본문 요약(미리보기) */
-  summary: string;
-}
