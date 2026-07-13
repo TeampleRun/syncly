f5b51c88e269f1388d21ea2d30c35c64731c0ba6
0011810 <hmgg285@gmail.com>
2026-07-13 17:29:28 +0900

    style: OTP/정보 입력 단계 이전 버튼 가운데 정렬 (#43)

--- src/views/signup/ui/SignupView.tsx
+++ src/views/signup/ui/SignupView.tsx
@@ -371,7 +371,7 @@ export default function SignupView() {
         )}
 
         {step !== 'done' && (
-          <div className="flex items-center justify-between">
+          <div className={`flex items-center ${step === 'email' ? 'justify-between' : 'justify-center'}`}>
             {step === 'email' && (
               <Link
                 href="/"
